4a09ea5532146ef37af2c83d8498860ba25688b0
Sandu Victor <vyctor661@gmail.com>
2021-07-31 22:34:41 +0300

    world map levels selector automatic generation

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,3 +1,25 @@
+import { route } from "preact-router";
+import { useEffect } from "preact/hooks";
+import { levelsList } from "../logic/levelsList";
+
+import "../styles/levelSelector.css";
+
+const GenerateLevelSelectorComponents = () => {
+    return levelsList.map((_, i) => {
+        return (
+            <div
+                className="levelSelector"
+                key={`levelSelector${i}`}
+                onClick={() => {
+                    route(`/levels/${i}`);
+                }}
+            >
+                {i + 1}
+            </div>
+        );
+    });
+};
+
 export default () => {
-    return <div>WorldMap hello</div>;
+    return <div>{GenerateLevelSelectorComponents()}</div>;
 };
