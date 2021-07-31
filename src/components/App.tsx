667404da1a0746d0bcaeea05e882e68c67643a95
Sandu Victor <vyctor661@gmail.com>
2021-07-31 21:53:40 +0300

    added a mini navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,39 @@
-import { Router, Route } from "preact-router";
+import { Router, Route, route } from "preact-router";
 
 import WorldMap from "./WorldMap";
 import NotFound from "./NotFound";
 import Homepage from "./Homepage";
 import Level from "./Level";
 
+import Home from "../icons/home";
+import LevelsIcon from "../icons/levels";
+
+import "../styles/app.css";
+
 export function App() {
     return (
-        <>
-            Navbarhere?
-            <Router>
-                <Route path="/" component={Homepage} />
-                <Route path="/levels" component={WorldMap} />
-                <Route path="/levels/:levelNumber" component={Level} />
-                <Route default component={NotFound} />
-            </Router>
-        </>
+        <div>
+            <div className="miniNavbar">
+                <Home
+                    onClick={() => {
+                        route("/", true);
+                    }}
+                ></Home>
+                <LevelsIcon
+                    onClick={() => {
+                        route("/levels", true);
+                    }}
+                ></LevelsIcon>
+            </div>
+
+            <div className="appContent">
+                <Router>
+                    <Route path="/" component={Homepage} />
+                    <Route path="/levels" component={WorldMap} />
+                    <Route path="/levels/:levelNumber" component={Level} />
+                    <Route default component={NotFound} />
+                </Router>
+            </div>
+        </div>
     );
 }
